c6e9d77444a5292be5f98cc7a7257e5e6215177c
Mohab Sarhan <mohab.sarhan17@users.noreply.github.com>
2020-11-21 14:40:45 +0200

    finishing homepage

---
+++ lib/pages/detailsrecipe.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailsRecipe extends StatefulWidget {
+  @override
+  _DetailsRecipeState createState() => _DetailsRecipeState();
+}
+
+class _DetailsRecipeState extends State<DetailsRecipe> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:recipe_demo/pages/detailsrecipe.dart';
 import 'package:recipe_demo/utilities/toolsutilites.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,13 +11,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _headerCover(),
-        _featuredSection()
-
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [_headerCover(), _featuredSection(),
+          _newSection()],
+      ),
     );
   }
 
@@ -25,10 +25,7 @@ class _HomePageState extends State<HomePage> {
       children: [
         Container(
           width: double.infinity,
-          height: MediaQuery
-              .of(context)
-              .size
-              .height * 0.3,
+          height: MediaQuery.of(context).size.height * 0.3,
           decoration: BoxDecoration(
               color: ToolsUtilities.mainColor,
               image: DecorationImage(
@@ -36,42 +33,47 @@ class _HomePageState extends State<HomePage> {
                   fit: BoxFit.cover)),
         ),
         Positioned(
-          bottom: 50, left: 22,
-          child: Column(
-              children: [
-                Text('Daily New Recipes', style: TextStyle(
-                  color: ToolsUtilities.whiteColor, fontSize: 18,),),
-                Text('Discovery Now', style: TextStyle(
+          bottom: 50,
+          left: 22,
+          child: Column(children: [
+            Text(
+              'Daily New Recipes',
+              style: TextStyle(
+                color: ToolsUtilities.whiteColor,
+                fontSize: 18,
+              ),
+            ),
+            Text('Discovery Now',
+                style: TextStyle(
                     color: ToolsUtilities.whiteColor,
                     fontWeight: FontWeight.bold,
                     fontSize: 22))
-              ]
-          ),
+          ]),
         )
       ],
     );
   }
 
-
   Widget _featuredSection() {
-    return
-    Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-    Padding(
-    padding: const EdgeInsets.only(left:10,top: 10,bottom: 10),
-    child: Text('Featured Recipe',style: TextStyle(
-    fontWeight: FontWeight.bold,
-    color: ToolsUtilities.secondColor,
-    fontSize: 25
-    ),),
-    ),
+        Padding(
+          padding: const EdgeInsets.only(left: 10, top: 10,),
+          child: Text(
+            'Featured Recipe',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: ToolsUtilities.secondColor,
+                fontSize: 25),
+          ),
+        ),
         Container(
-          height: MediaQuery.of(context).size.height*0.4,
+          height: MediaQuery.of(context).size.height * 0.4,
           child: ListView.builder(
-            scrollDirection: Axis.horizontal,
+              scrollDirection: Axis.horizontal,
               itemCount: ToolsUtilities.imageRecipes.length,
-              itemBuilder: (context,index){
+              itemBuilder: (context, index) {
                 return _featuredCard(index);
               }),
         )
@@ -79,41 +81,125 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _featuredCard(int index){
-    return Stack(
-      children: [
-        Container(
-          margin: EdgeInsets.all(8),
-          width: 150,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-              color: ToolsUtilities.mainColor,
-              image: DecorationImage(
-                  image: NetworkImage(ToolsUtilities.imageRecipes[index]),
-                  fit: BoxFit.cover)),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Title Recipe', style: TextStyle(
-                  color: ToolsUtilities.whiteColor, fontSize: 18,),),
-                Row(
-                  children: [
-                    Icon(Icons.restaurant,size: 15,color: ToolsUtilities.whiteColor,),
-                    SizedBox(width: 5),
-                    Text('Category', style: TextStyle(
-                      color: ToolsUtilities.whiteColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20)),
-                  ]
-                )
-              ]
+  Widget _featuredCard(int index) {
+    return InkWell(
+      onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> DetailsRecipe()));},
+      child: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.all(8),
+            width: MediaQuery.of(context).size.width * 0.4,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: ToolsUtilities.mainColor,
+                image: DecorationImage(
+                    image: NetworkImage(ToolsUtilities.imageRecipes[index]),
+                    fit: BoxFit.cover)),
           ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Title Recipe',
+                    style: TextStyle(
+                      color: ToolsUtilities.whiteColor,
+                      fontSize: 17,
+                    ),
+                  ),
+                  Row(children: [
+                    Icon(
+                      Icons.restaurant,
+                      size: 15,
+                      color: ToolsUtilities.whiteColor,
+                    ),
+                    SizedBox(width: 5),
+                    Text('Category',
+                        style: TextStyle(
+                            color: ToolsUtilities.whiteColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17)),
+                  ])
+                ]),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _newSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10, top: 10,),
+          child: Text(
+            'New ',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: ToolsUtilities.secondColor,
+                fontSize: 25),
+          ),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.24,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: ToolsUtilities.imageRecipes.length,
+              itemBuilder: (context, index) {
+                return _newCard(index);
+              }),
         )
       ],
+    );
+  }
+  Widget _newCard(int index){
+    return InkWell(
+      onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> DetailsRecipe()));},
+      child: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.all(8),
+            width: MediaQuery.of(context).size.width * 0.7,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: ToolsUtilities.mainColor,
+                image: DecorationImage(
+                    image: NetworkImage(ToolsUtilities.imageRecipes[index]),
+                    fit: BoxFit.cover)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Title Recipe',
+                    style: TextStyle(
+                      color: ToolsUtilities.whiteColor,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Row(children: [
+                    Icon(
+                      Icons.timer,
+                      size: 17,
+                      color: ToolsUtilities.whiteColor,
+                    ),
+                    SizedBox(width: 5),
+                    Text('60 minutes',
+                        style: TextStyle(
+                            color: ToolsUtilities.whiteColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17)),
+                  ])
+                ]),
+          )
+        ],
+      ),
     );
   }
 }
