8048f67c6da10e7a93763f119e16ea6f1f3dfa56
Mohab Sarhan <mohab.sarhan17@users.noreply.github.com>
2020-11-21 12:46:20 +0200

    adding HomePage Header

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -9,8 +9,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor:Colors.green,
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.3,
+              decoration: BoxDecoration(
+                  color: ToolsUtilities.mainColor,
+                  image: DecorationImage(
+                      image: NetworkImage(ToolsUtilities.imageRecipes[4]),
+                      fit: BoxFit.cover)),
+            ),
+            Positioned(
+              bottom: 50,left: 22,
+              child: Column(
+                children: [
+                  Text('Daily New Recipes',style: TextStyle(color: ToolsUtilities.whiteColor,fontSize: 18,),),
+                  Text('Discovery Now',style: TextStyle(color: ToolsUtilities.whiteColor,fontWeight: FontWeight.bold,fontSize: 22))
+                ]
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
